e0ae77b0cc66fe48467670c999d0ed273d97aac9
Le Zhou <zhoule@abakusglobal.com>
2025-08-01 11:27:50 +0800

    03,04 fixed

--- src/bus.zig
+++ src/bus.zig
@@ -44,18 +44,36 @@ pub const Bus = struct {
             0x8000...0x9FFF => return self.vram[address - 0x8000],
             0xA000...0xBFFF => return self.external_ram[address - 0xA000],
             0xC000...0xDFFF => return self.wram[address - 0xC000],
-            0xE000...0xFDFF => return self.read(address - 0x2000),
+            0xE000...0xFDFF => return self.read(address - 0x2000), // ECHO
             0xFE00...0xFE9F => return self.oam[address - 0xFE00],
             0xFEA0...0xFEFF => return 0xFF,
             0xFF80...0xFFFE => return self.hram[address - 0xFF80],
             0xFFFF => return self.interrupt_enable_register,
-            0xFF04 => return self.timer.div,
-            0xFF05 => return self.timer.tima,
-            0xFF06 => return self.timer.tma,
-            0xFF07 => return self.timer.tac,
-            0xFF44 => return self.ppu.ly,
-            0xFF0F => return self.interrupt_flag,
-            else => return self.io_registers[address - 0xFF00],
+            0xFF00...0xFF7F => {
+                switch (address) {
+                    0xFF04 => return @truncate(self.timer.div_counter >> 8),
+                    0xFF05 => return self.timer.tima,
+                    0xFF06 => return self.timer.tma,
+                    0xFF07 => return self.timer.tac,
+                    0xFF44 => return self.ppu.ly,
+                    0xFF0F => return self.interrupt_flag,
+                    0xFF01 => {
+                        // std.log.debug("Read SB (Serial Data): 0x{x:0>2}", .{self.io_registers[0x01]});
+                        return self.io_registers[0x01];
+                    },
+                    0xFF02 => {
+                        // std.log.debug("Read SC (Serial Control): 0x{x:0>2}", .{self.io_registers[0x02]});
+                        return self.io_registers[0x02];
+                    },
+                    else => {
+                        const offset = address - 0xFF00;
+                        if (offset < self.io_registers.len) {
+                            return self.io_registers[offset];
+                        }
+                        return 0xFF;
+                    },
+                }
+            },
         }
     }
     pub fn write(self: *Bus, address: u16, value: u8) void {
@@ -72,48 +90,74 @@ pub const Bus = struct {
             0xFF00...0xFF7F => {
                 switch (address) {
                     0xFF04 => {
-                        self.timer.div = 0;
                         self.timer.div_counter = 0;
                         return;
                     },
                     0xFF05 => {
                         self.timer.tima = value;
+                        // std.log.debug("Write TIMA=0x{x:0>2}", .{value});
                         return;
                     },
                     0xFF06 => {
                         self.timer.tma = value;
+                        // std.log.debug("Write TMA=0x{x:0>2}", .{value});
                         return;
                     },
                     0xFF07 => {
                         self.timer.tac = value;
+                        // std.log.debug("Write TAC=0x{x:0>2}", .{value});
                         return;
                     },
                     0xFF0F => {
                         self.interrupt_flag = value;
+                        // std.log.debug("Write IF=0x{x:0>2}", .{value});
                         return;
                     },
                     0xFF01 => {
                         self.io_registers[0x01] = value;
+                        // std.log.debug("Write SB (Serial Data)=0x{x:0>2} ('{c}')", .{ value, if (value >= 32 and value < 127) value else '?' });
                         return;
                     },
                     0xFF02 => {
                         if (value == 0x81) {
                             const char = self.io_registers[0x01];
-                            std.debug.print("{c}", .{char});
+                            std.io.getStdOut().writer().print("{c}", .{char}) catch |err| {
+                                std.log.err("Error printing char to serial: {s}", .{@errorName(err)});
+                            };
                             // Transfer complete, clear bit 7
-                            self.io_registers[0x02] = 0x01;
+                            self.io_registers[address - 0xFF00] = value & 0x7F;
                         } else {
-                            self.io_registers[0x02] = value;
+                            self.io_registers[address - 0xFF00] = value;
                         }
                         return;
                     },
-                    0xFF44 => {},
+                    0xFF44 => {
+                        return;
+                    },
                     0xFF46 => {
                         self.dma_transfer(value);
+                        return;
                     },
-                    else => {},
+                    0xFF40 => { // LCDC
+                        const old_val = self.io_registers[0x40];
+                        const new_val = value;
+                        self.io_registers[0x40] = new_val;
+
+                        const old_enabled = (old_val & 0x80) != 0;
+                        const new_enabled = (new_val & 0x80) != 0;
+
+                        if (!old_enabled and new_enabled) {
+                            self.ppu.enabled = true;
+                        } else if (old_enabled and !new_enabled) {
+                            self.ppu.reset();
+                        }
+                        return;
+                    },
+                    else => {
+                        self.io_registers[address - 0xFF00] = value;
+                        return;
+                    },
                 }
-                self.io_registers[address - 0xFF00] = value;
             },
         }
     }
@@ -131,7 +175,7 @@ pub const Bus = struct {
                 0x8000...0x9FFF => self.vram[current_source_addr - 0x8000],
                 0xA000...0xBFFF => self.external_ram[current_source_addr - 0xA000],
                 0xC000...0xDFFF => self.wram[current_source_addr - 0xC000],
-                0xE000...0xFDFF => self.wram[current_source_addr - 0x2000],
+                0xE000...0xFDFF => self.wram[current_source_addr - 0xE000],
                 else => 0xFF,
             };
             self.oam[i] = value;
